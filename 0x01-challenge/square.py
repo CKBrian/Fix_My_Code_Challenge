d4958d121e26fa3fa3decd3b9e8aa85a124607ed
Brian <chumobrian@gmail.com>
2024-01-31 01:54:15 +0300

    chore: remove extra spaces in code and add a comment

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
-""" calculates area and perimeter and returns a square string """
-
 
 class square():
-    """ calculates area and perimeter and returns a square string """
+    
     width = 0
     height = 0
 
+    
     def __init__(self, *args, **kwargs):
-        """ initializes a square class """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -17,16 +15,13 @@ class square():
         return self.width * self.height
 
     def PermiterOfMySquare(self):
-        """ Perimeter of the square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        """ returns a square string """
         return "{}/{}".format(self.width, self.height)
 
-
 if __name__ == "__main__":
-    """ Creates a square class """
+
     s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
